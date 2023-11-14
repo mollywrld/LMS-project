35587c97f98822755bdd1e6d6ae96d45a0f9d79e
mollywrld <itspavan2003@gmail.com>
2023-11-15 00:00:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CoursesBiz from './components/CoursesBiz';
+import Courses from './components/Courses';
+import CoursesLife from './components/CoursesLife';
+import CourseView from './components/CourseView';
+import {BrowserRouter,Routes, Route,} from 'react-router-dom';
+import CourseviewLife from './components/CourseviewLife';
+import CourseviewBiz from './components/CourseviewBiz';
+import NotFound from './components/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path='/courseview' element={<CourseView/>}></Route>
+      <Route path='/courseview-lifestyle' element={<CourseviewLife/>}></Route>
+      <Route path='/courseview-business' element={<CourseviewBiz/>}></Route>
+      <Route path='/tech' exact Component={Courses}/>
+      <Route path='/business' exact Component={CoursesBiz}/>
+      <Route path='/lifestyle' exact Component={CoursesLife}/>
+      <Route path='/*' exact Component={NotFound}/>
+      
+    </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
+
